07ada37da98befbac4a24454f3f47215e0ac7b89
elois-matas <elois-matas@gmx.com>
2025-03-05 17:58:19 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int num1 = 4;
+    int num2 = 5;
+    std::cout << "The sum of " << num1 << " and " << num2 << " is " << num1 + num2 << std::endl;
+    return 0;
+}
